014e68beb4e23704c9eb0871b5a4c3eb00ba8364
Sergey Suslov <sergey.suslov.nsk@gmail.com>
2019-07-20 17:51:06 +0700

    error handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import Koa from 'koa'
+import koaBody from 'koa-body'
+import session from 'koa-session'
 import logger from './logger'
+import errorHandler from './middlewares/error-handler'
 
 const app = new Koa()
 app.context.log = logger
 
+app.keys = [process.env.SESSION_SECRET]
+
+const CONFIG = {
+  maxAge: 86400000
+}
+
+app.use(session(CONFIG, app))
+app.use(koaBody({ multipart: true }))
+app.use(errorHandler)
 app.use((ctx, next) => {
   ctx.log.trace(`[${ctx.method}] - ${ctx.url}`)
   ctx.body = 'suc'
-  next()
 })
 
 app.listen(3000)
