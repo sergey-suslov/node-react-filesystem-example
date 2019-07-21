1ca15ee2a230894ecbd3649171598eaf1ecf4960
Sergey Suslov <sergey.suslov.nsk@gmail.com>
2019-07-21 18:15:31 +0700

    sensetive info log removed

--- server/src/routes/api/user/profile.js
+++ server/src/routes/api/user/profile.js
@@ -1,8 +1,6 @@
 import _ from 'lodash'
 
 const profile = async ctx => {
-  ctx.log.info(ctx.cookies.get('token'))
-  ctx.log.info(ctx.state)
   ctx.body = _.pick(ctx.state.user, ['_id', 'email'])
 }
 
